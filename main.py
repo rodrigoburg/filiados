beedc4c2454880e0a95a6c11ac4983d9dc1d8575
rodrigoburg <rodrigo.burgarelli1@gmail.com>
2015-06-17 15:58:18 -0300

    mais detalhadinho

--- main.py
+++ main.py
@@ -142,6 +142,130 @@ def conta_filiados_mes():
     with open('filiados_2015_dia.txt', 'w') as outfile:
         json.dump(saida, outfile)
 
+def conta_estoque_ano():
+    arquivos = le_arquivos(".")
+    #if "partido_ano_filiados.json" in arquivos and "partido_ano_desfiliados.json" in arquivos:
+    if "partido_ano_filiados2011.json" in arquivos and "partido_ano_filiados2011.json" in arquivos:
+        print("Arquivos preparatórios foram encontrados! Vamos direto fazer o cálculo")
+        termina_contagem_estoque()
+    else:
+        print("Não temos os arquivos preparatórios. Vamos montá-los do zero")
+        prepara_contagem_estoque()
+        termina_contagem_estoque()
+
+def prepara_contagem_estoque():
+    conexao = conecta("filiados")
+    dados = conexao.find({},{'DATA DA FILIACAO':1,'SITUACAO DO REGISTRO':1,"_id":0,"SIGLA DO PARTIDO":1,"DATA DA DESFILIACAO":1,"DATA DO CANCELAMENTO":1})
+
+    filiados = {}
+    desfiliados = {}
+    saida = {}
+    #anos = range(1979,2016)
+    anos = range(1979,2012)
+    for a in anos:
+        filiados[a] = {}
+        desfiliados[a] = {}
+        saida[a] = {}
+
+    i = 0
+    for d in dados:
+        i += 1
+        if i % 50000 == 0:
+            print("ESTAMOS NA LINHA "+str(i))
+
+        data = [int(a) for a in d["DATA DA FILIACAO"].split("/")]
+        dia_filiacao = data[0]
+        mes_filiacao = data[1]
+        ano_filiacao = data[2]
+
+        if d["DATA DA DESFILIACAO"]:
+            data = [int(a) for a in d["DATA DA DESFILIACAO"].split("/")]
+            ano_desfiliacao = data[2]
+        else:
+            if d["DATA DO CANCELAMENTO"]:
+                data = [int(a) for a in d["DATA DO CANCELAMENTO"].split("/")]
+                ano_desfiliacao = data[2]
+            else:
+                ano_desfiliacao = False
+
+        #if (1979 < ano_filiacao < 2016 and (not ano_desfiliacao or 1979 < ano_desfiliacao < 2016)):
+        if (1979 < ano_filiacao < 2012 and (not ano_desfiliacao or 1979 < ano_desfiliacao < 2012)):
+            if (not (ano_filiacao == 2011 and mes_filiacao > 4)) or (not (ano_filiacao == 2011 and mes_filiacao == 4 and dia_filiacao>14)):
+                if not d["SIGLA DO PARTIDO"] in filiados[ano_filiacao]:
+                    filiados[ano_filiacao][d["SIGLA DO PARTIDO"]] = 0
+                filiados[ano_filiacao][d["SIGLA DO PARTIDO"]] += 1
+
+                if ano_desfiliacao:
+                    if not d["SIGLA DO PARTIDO"] in desfiliados[ano_desfiliacao]:
+                        desfiliados[ano_desfiliacao][d["SIGLA DO PARTIDO"]] = 0
+                    desfiliados[ano_desfiliacao][d["SIGLA DO PARTIDO"]] += 1
+
+    #with open('partido_ano_filiados.json', 'w') as outfile:
+    with open('partido_ano_filiados2011.json', 'w') as outfile:
+        json.dump(filiados, outfile)
+
+    #with open('partido_ano_desfiliados.json', 'w') as outfile:
+    with open('partido_ano_desfiliados2011.json', 'w') as outfile:
+        json.dump(desfiliados, outfile)
+
+
+def termina_contagem_estoque():
+    #with open('partido_ano_filiados.json', 'r') as jsonfile:
+    with open('partido_ano_filiados2011.json', 'r') as jsonfile:
+        filiados = json.load(jsonfile)
+
+    #with open('partido_ano_desfiliados.json', 'r') as jsonfile:
+    with open('partido_ano_desfiliados2011.json', 'r') as jsonfile:
+        desfiliados = json.load(jsonfile)
+
+    #primeiro, colocamos 0 filiados para os partidos que não tiveram filiados em um determinado ano
+    partidos = []
+    for ano in filiados:
+        for sigla in filiados[ano]:
+            if sigla not in partidos:
+                partidos.append(sigla)
+
+    for p in partidos:
+        for ano in filiados:
+            if p not in filiados[ano]:
+                filiados[ano][p] = 0
+
+    #agora calculamos o saldo ano a ano
+    saida = {}
+    for ano in filiados:
+        if ano not in saida:
+            saida[ano] = {}
+        for sigla in filiados[ano]:
+            if sigla in desfiliados[ano]:
+                saida[ano][sigla] = filiados[ano][sigla] - desfiliados[ano][sigla]
+            else:
+                saida[ano][sigla] = filiados[ano][sigla]
+
+    #e depois do saldo, o estoque
+    estoque = {}
+    for ano in saida:
+        estoque[ano] = {}
+        for sigla in saida[ano]:
+            estoque[ano][sigla] = soma_anos_anteriores(saida,ano,sigla)
+
+    df = DataFrame(estoque)
+    print(df)
+
+    df = df.fillna(0)
+    #df.to_csv("estoque_partidos_ano.csv")
+    df.to_csv("estoque_partidos_ano2011.csv")
+
+def soma_anos_anteriores(dados,ano,sigla):
+    anos = [int(k) for k in dados.keys()]
+    primeiro = min(anos)
+    saida = 0
+
+    for a in range(primeiro,int(ano)+1):
+        ano_atual = str(a)
+        if sigla in dados[ano_atual]:
+            saida += dados[ano_atual][sigla]
+    return saida
+
 def conta_filiados_total():
     conexao = conecta("filiados")
     dados = conexao.find()
@@ -201,8 +325,9 @@ def analisa_json():
 #baixa_dados()
 #descompacta(): #roda na pasta: #unzip \*.zip
 #sobe_bd()
-conta_dias()
+#conta_dias()
 #conta_filiados_mes()
 #conta_filiados_total()
 #analisa_json()
+conta_estoque_ano()
 
